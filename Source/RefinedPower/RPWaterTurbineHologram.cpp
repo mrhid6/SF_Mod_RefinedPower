b06cb8828bdc1e7b5dc2c2abcd68513282c5d06d
Mrhid6 <mrhid6@gmail.com>
2020-03-18 23:58:29 +0000

    Sorta Works?

--- Source/RefinedPower/RPWaterTurbineHologram.cpp
+++ Source/RefinedPower/RPWaterTurbineHologram.cpp
@@ -7,17 +7,73 @@
 #include "Containers/UnrealString.h"
 #include "Kismet/GameplayStatics.h"
 #include "UObject/WeakObjectPtr.h"
+#include "Components/BoxComponent.h"
 
-//bool ARPWaterTurbineHologram::IsValidHitResult(const FHitResult& hitResult) const {
-//	Super::IsValidHitResult(hitResult);
-//
-//	if (hitResult.Actor.IsValid()) {
-//		if (Cast<AFGWaterVolume>(hitResult.Actor)) {
-//			return false;
-//		}
-//	}
-//	return true;
-//}
+ARPWaterTurbineHologram::ARPWaterTurbineHologram() {
+	WaterTest = CreateDefaultSubobject<UBoxComponent>(TEXT("WaterTest"));
+	WaterTest->SetupAttachment(RootComponent);
+
+	FVector boxSize = FVector(100, 100, 100);
+	WaterTest->SetBoxExtent(boxSize);
+
+	WaterTest->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+}
+
+bool ARPWaterTurbineHologram::IsValidHitResult(const FHitResult& hitResult) const {
+	Super::IsValidHitResult(hitResult);
+
+	return SetOverlapWaterVolume();
+}
+
+bool ARPWaterTurbineHologram::SetOverlapWaterVolume() const {
+
+	ARPWaterTurbineHologram* ptr = const_cast<ARPWaterTurbineHologram*>(this);
+
+	TArray<AActor*> foundWater;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AFGWaterVolume::StaticClass(), foundWater);
+
+	for (int i = 0; i < foundWater.Num(); i++)
+	{
+		AFGWaterVolume* water = (AFGWaterVolume*)foundWater[i];
+
+		// This WORKS!
+		if (WaterTest->IsOverlappingActor(water)) {
+			SML::Logging::info("[RefinedPower] - Valid Water Volume");
+
+
+			// CANT SET THIS!!!
+			ptr->FoundWater = water;
+			return true;
+		}
+	}
+
+	ptr->FoundWater = nullptr;
+	return false;
+}
+
+
+
+void ARPWaterTurbineHologram::SetHologramLocationAndRotation(const FHitResult& hitResult) {
+	
+	// Some weird shit going on here! probs IF statement?
+
+	if (!FoundWater) {
+		Super::SetHologramLocationAndRotation(hitResult);
+	}
+	else {
+		SML::Logging::info("[RefinedPower] - Set Location to WATER?");
+		FVector WaterOrigin;
+		FVector WaterBounds;
+		FoundWater->GetActorBounds(false, WaterOrigin, WaterBounds);
+
+		float WaterZ = WaterOrigin.Z;
+		WaterZ += WaterBounds.Z;
+
+		FVector location = FVector(hitResult.ImpactPoint.X, hitResult.ImpactPoint.Y, WaterZ);
+
+		this->SetActorLocation(location, false);
+	}
+}
 
 
 void ARPWaterTurbineHologram::BeginPlay() {
@@ -43,32 +99,6 @@ void ARPWaterTurbineHologram::testScript() {
 
 		SML::Logging::info("[RefinedPower] - ", className, " ", objectName);
 	}*/
-}
-
-bool ARPWaterTurbineHologram::IsValidHitResult(const FHitResult& hitResult) const {
-	Super::IsValidHitResult(hitResult);
-
-	if (hitResult.Component.IsValid()) {
-		AFGWaterVolume* p = (AFGWaterVolume*)(hitResult.Component).Get();
-		if (p && !p->IsA((UClass*)AFGWaterVolume::StaticClass())) {
-			SML::Logging::info("[RefinedPower] - Valid Water Volume");
-			return true;
-		}
-
-
-		/*TArray<AActor*> foundWater;
-		UGameplayStatics::GetAllActorsOfClass(GetWorld(), AFGWaterVolume::StaticClass(), foundWater);
-		for(int i = 0; i < foundWater.Num(); i++)
-		{
-			AFGWaterVolume* water = (AFGWaterVolume*)foundWater[i];
-			if (this->IsOverlappingActor(water)) {
-				SML::Logging::info("[RefinedPower] - Valid Water Volume");
-				return true;
-			}
-		}*/
-	}
-	SML::Logging::info("[RefinedPower] - Invalid Water Volume");
-	return false;
 }
 //void ARPWaterTurbineHologram::CheckValidPlacement() {
 //	Super::CheckValidPlacement();
--- Source/RefinedPower/RPWaterTurbineHologram.h
+++ Source/RefinedPower/RPWaterTurbineHologram.h
@@ -3,19 +3,34 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Hologram/FGFactoryHologram.h"
+#include "Hologram/FGBuildableHologram.h"
+#include "Components/BoxComponent.h"
+#include "FGWaterVolume.h"
 #include "RPWaterTurbineHologram.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class REFINEDPOWER_API ARPWaterTurbineHologram : public AFGFactoryHologram
+class REFINEDPOWER_API ARPWaterTurbineHologram : public AFGBuildableHologram
 {
 	GENERATED_BODY()
 
+		ARPWaterTurbineHologram();
+public:
 	virtual void BeginPlay() override;
 	virtual bool IsValidHitResult(const FHitResult& hitResult) const override;
+	virtual void SetHologramLocationAndRotation(const FHitResult& hitResult) override;
 	//void CheckValidPlacement() override;
 	void testScript();
+
+	bool SetOverlapWaterVolume() const;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		class UBoxComponent* WaterTest;
+
+protected:
+
+	AFGWaterVolume* FoundWater;
+
 };
